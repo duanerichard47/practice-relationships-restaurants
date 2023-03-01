6fb464257b68ff3674618a314837613a78109503
Duane <duanerichard47@hotmail.com>
2023-03-01 12:11:47 -0500

    14 finishes

--- src/customer.py
+++ src/customer.py
@@ -1,2 +1,33 @@
+from review import Review #  got this wrong
+
 class Customer:
-    pass
+   
+    
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.reviews =[]
+        
+
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.last_name
+    
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_reviews(self):
+        return self.reviews
+    
+    def add_review(self, restaurant,rating):
+        # return self.reviews[0].get.rating ==5  got this wrong
+        review = Review(self, restaurant, rating)
+        self.reviews.append(review)
+        return self.reviews
+
+    def review_count(self):
+            # len(self.reviews) == 1  got this wrong
+            # return self.reviews
+        return len(self.reviews)
--- src/restaurant.py
+++ src/restaurant.py
@@ -1,2 +1,10 @@
 class Restaurant:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.reviews =[]
+
+    def get_name(self):
+        return self.name 
+    
+    def get_reviews(self):
+        return self.reviews
